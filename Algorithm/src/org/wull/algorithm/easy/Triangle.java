4ef91f190a8be83fd6ea855061fa2e658f000c3c
wu <whhrf649@163.com>
2015-03-23 23:16:59 +0800

    Improvement

--- Algorithm/src/org/wull/algorithm/easy/Triangle.java
+++ Algorithm/src/org/wull/algorithm/easy/Triangle.java
@@ -9,22 +9,16 @@ public class Triangle {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println(Triangle.generate(5));
 	}
 	
-	public List<List<Integer>> generate(int numRows) {
+	public static List<List<Integer>> generate(int numRows) {
     	List<List<Integer>> results = new ArrayList<List<Integer>>();
-    	if(numRows<1){
-    		return results;
-    	}
+
         for(int i=0;i<numRows;i++){
         	List<Integer> list = new ArrayList<Integer>();
         	if(i==0){
-        		list.add(1);
-        	}else if(i==1){
-        		list.add(1);
-        		list.add(1);
+        		list.add(1);        		
         	}else{
         		list.add(1);
         		for(int j=0;j<i-1;j++){
