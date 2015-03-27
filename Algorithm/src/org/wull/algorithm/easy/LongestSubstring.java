cb6fb35df965b9c7102bd7473631dfa1ff57ff81
wuhan <wuhan@ebupt.com>
2015-03-27 16:21:11 +0800

    Given a string, find the length of the longest substring without repeating characters. For example, the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3. For "bbbbb" the longest substring is "b", with the length of 1.

---
+++ Algorithm/src/org/wull/algorithm/easy/LongestSubstring.java
@@ -0,0 +1,62 @@
+package org.wull.algorithm.easy;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class LongestSubstring {
+
+	public static void main(String[] args) {
+		long t1 = System.currentTimeMillis();
+		for(int i=0;i<1;i++)
+		System.out.println(lengthOfLongestSubstring("abba"));
+		long t2 = System.currentTimeMillis();
+		System.out.println(t2-t1);
+	}
+
+	public static int lengthOfLongestSubstring(String s) {
+		Map<Character,Integer> hs = new HashMap<Character,Integer>();
+		int max = 0;
+		int temp =0;
+		if(s==null){
+			return 0;
+		}
+		for(int i=0;i<s.length();i++){
+			Character ch = s.charAt(i);
+			temp++;
+			if(hs.containsKey(ch)){
+				temp = i-hs.get(ch);
+			}
+			if(temp>max){
+				max = temp;
+			}
+			hs.put(ch, i);
+		}
+		return max;
+	}
+	
+	public static int lengthOfLongestSubstring2(String s) {
+		//abcda  abcd
+		Set<Character> hs = new HashSet<Character>();
+		int size=0;
+		int max = 0;
+		if(s==null){
+			return 0;
+		}
+		for(int i=0;i<s.length();i++){
+			Character ch = s.charAt(i);
+			if(hs.contains(ch)){
+				if(size>max) {
+					max=size;
+				}
+				size=0;
+				hs.clear();
+			}
+			size++;
+			hs.add(ch);
+		}
+		return max;
+	}
+
+}
